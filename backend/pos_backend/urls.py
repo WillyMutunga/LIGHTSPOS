2ebf074b5d7bc69bcceb9526868396c5eac090ba
Willy Mutunga <wsharks003@gmail.com>
2026-07-25 16:37:26 +0300

    Temporary endpoint to run DB migrations via web

--- backend/pos_backend/urls.py
+++ backend/pos_backend/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
+from django.http import HttpResponse
+from django.core.management import call_command
+
+def run_migrations(request):
+    try:
+        call_command('migrate')
+        return HttpResponse("Database migrations successfully applied!")
+    except Exception as e:
+        return HttpResponse(f"Migration failed: {str(e)}", status=500)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/run-secret-migrations-willy-123/', run_migrations),
     path('', include('store.urls')),
 ]
