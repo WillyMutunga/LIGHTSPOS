efc063ce20466180f4c605c477db5636afd5cd76
Willy Mutunga <wsharks003@gmail.com>
2026-08-05 15:38:48 +0300

    fix: add sys.path.insert to migrate_db.py to resolve ModuleNotFoundError

--- backend/migrate_db.py
+++ backend/migrate_db.py
@@ -1,7 +1,9 @@
 import os
+import sys
 import django
 from django.core.management import call_command
 
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lightspos_backend.settings')
 django.setup()
 
